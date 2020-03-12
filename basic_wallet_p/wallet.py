45940344b492a5469be5fc87389cc8249a28f6db
LenWinkler <lenwinkler86@gmail.com>
2020-03-12 18:50:26 -0400

    wallet CLI is working

--- basic_wallet_p/wallet.py
+++ basic_wallet_p/wallet.py
@@ -5,7 +5,22 @@ import json
 user_id = input('Enter your user id: ')
 balance = 0
 
-r = requests.get(url="http://localhost:5000/chain")
-chain = r.json()
+r = requests.get(url='http://localhost:5000/chain')
 
-print('chain: ', chain)
+raw_chain = r.json()
+# convert to str
+str_chain = json.dumps(raw_chain['chain'])
+# convert to json
+json_chain = json.loads(str_chain)
+
+
+for block in json_chain:
+    
+    if len(block['transactions']) > 0:
+        transactions = block['transactions']
+        for item in transactions:
+            print('item', item)
+            if item['recipient'] == user_id or item['recipient'] == user_id + '\n':
+                balance += int(item['amount'])
+
+print(f'Your account balance is {balance} coin(s)')
