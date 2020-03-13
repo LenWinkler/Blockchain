bd086842e4ff170970ba0a1843bf595d7a18653d
LenWinkler <lenwinkler86@gmail.com>
2020-03-12 20:37:51 -0400

    wallet print transactions. hit mvp

--- basic_wallet_p/wallet.py
+++ basic_wallet_p/wallet.py
@@ -21,6 +21,11 @@ else:
         i.close()
 # balance counter
 balance = 0
+users_transactions = []
+
+def print_transactions(transactions):
+    for item in transactions:
+        print(f'\n{item}\n')
 
 r = requests.get(url='http://localhost:5000/chain')
 
@@ -36,8 +41,14 @@ for block in json_chain:
     if len(block['transactions']) > 0:
         transactions = block['transactions']
         for item in transactions:
-            print('item', item)
+            # add relevant transactions to users_transactions
+            if item['recipient'] == user_id or item['sender'] == user_id:
+                users_transactions.append(item)
+            # if user is recipient in this transaction, amount to balance
             if item['recipient'] == user_id or item['recipient'] == user_id + '\n':
                 balance += int(item['amount'])
 
-print(f'{user_id}\'s account balance is {balance} coin(s)')
+print(f'{user_id}\'s account balance is {balance} coin(s)\n')
+print(f'{user_id}\'s transactions: \n')
+print_transactions(users_transactions)
+print('\n')
